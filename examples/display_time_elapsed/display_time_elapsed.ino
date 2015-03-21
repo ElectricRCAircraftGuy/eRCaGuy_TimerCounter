d2d80f90a8d52b3af7e0c80bd5cdde3998d75b1a
Gabriel Staples <ercaguy@gmail.com>
2015-03-21 17:58:05 -0400

    Changed license from GLP to LGPL V3 or later

--- examples/display_time_elapsed/display_time_elapsed.ino
+++ examples/display_time_elapsed/display_time_elapsed.ino
@@ -14,25 +14,25 @@ Updated: 30 May 2014
 /*
 ===================================================================================================
   LICENSE & DISCLAIMER
-  Copyright (C) 2014 Gabriel Staples.  All right reserved.
+  Copyright (C) 2015 Gabriel Staples.  All right reserved.
   
   ------------------------------------------------------------------------------------------------
-  License: GNU General Public License Version 3 (GPLv3) - https://www.gnu.org/licenses/gpl.html
+  License: GNU Lesser General Public License Version 3 (LGPLv3) or later - https://www.gnu.org/licenses/lgpl.html
   ------------------------------------------------------------------------------------------------
 
   This file is part of eRCaGuy_Timer2_Counter.
   
   This program is free software: you can redistribute it and/or modify
-  it under the terms of the GNU General Public License as published by
+  it under the terms of the GNU Lesser General Public License as published by
   the Free Software Foundation, either version 3 of the License, or
   (at your option) any later version.
 
   This program is distributed in the hope that it will be useful,
   but WITHOUT ANY WARRANTY; without even the implied warranty of
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-  GNU General Public License for more details.
+  GNU Lesser General Public License for more details.
 
-  You should have received a copy of the GNU General Public License
+  You should have received a copy of the GNU Lesser General Public License
   along with this program.  If not, see http://www.gnu.org/licenses/
 ===================================================================================================
 */
